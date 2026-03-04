011432ea276c5c6d2bd1cdabb321580a08b7b90a
Bhuvaneswari1606 <bhuvaneswarimagesh160605@gmial.com>
2026-03-04 16:15:05 +0530

    Smart DB: MySQL local + SQLite Vercel

--- database.py
+++ database.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/store_support_ai"
+if os.getenv("VERCEL") or os.getenv("VERCEL_URL"):
+    DATABASE_URL = "sqlite:///store_support_ai.db"  # Vercel
+else:
+    DATABASE_URL = "mysql+pymysql://root:@localhost/store_support_ai"  # Local
 
 engine = create_engine(DATABASE_URL)
 

2e8303827071056189d85652ed76f9bc8c13bbf5
Bhuvaneswari1606 <bhuvaneswarimagesh160605@gmial.com>
2026-03-06 11:49:26 +0530

    String store_id + Aiven ready

--- models/user.py
+++ models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String,Base
+from sqlalchemy import Column, Integer, String
+from . import Base  
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
